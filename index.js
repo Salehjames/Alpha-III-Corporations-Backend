169979ad13e220a2a4927a89ace9986b8c976054
James Saleh <105816905+Salehjames@users.noreply.github.com>
2025-02-13 17:28:16 +0000

    new commit

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ import app from "./app.js";
 
 ( async () => {
     try {
+        await mongoose.connect("mongodb://localhost:27017/alphaIIIcorporations")
+        console.log("DB CONNECTED ");
 
+        const onListening = () => {
+            console.log("listening on PORT 5000");
+        }
+
+        app.listen(5000, onListening)
     } catch (error) {
-        
+        console.error("error ", error);
+        throw err;
     }
 })()
